f32423ff07d9d61698e10358e34d3b87399b920c
Adrien KISSIE <fredkiss3@gmail.com>
2023-07-15 20:01:11 +0200

    ♻️ theme should be lowercase

--- src/app/(actions)/theme.ts
+++ src/app/(actions)/theme.ts
@@ -5,11 +5,10 @@ import { z } from "zod";
 import { THEME_COOKIE_KEY } from "~/lib/constants";
 
 const themeSchema = z
-  .union([z.literal("DARK"), z.literal("LIGHT"), z.literal("SYSTEM")])
+  .union([z.literal("dark"), z.literal("light"), z.literal("system")])
   .optional()
-  .default("SYSTEM")
-  .catch("SYSTEM")
-  .transform((arg) => arg.toLowerCase() as Lowercase<typeof arg>);
+  .default("system")
+  .catch("system");
 
 export type Theme = z.TypeOf<typeof themeSchema>;
 
