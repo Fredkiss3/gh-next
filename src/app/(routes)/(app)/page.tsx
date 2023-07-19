b11a10b78bf7a4002e6dd99aeb17f830b04fdac0
Adrien KISSIE <fredkiss3@gmail.com>
2023-07-19 09:52:25 +0200

    ♻️ use fork link for fork button

--- src/app/(routes)/(app)/page.tsx
+++ src/app/(routes)/(app)/page.tsx
@@ -65,7 +65,7 @@ export default async function Page() {
             <CounterBadge count={repositoryStats.watcherCount} />
           </Button>
           <Button
-            href="https://github.com/Fredkiss3/gh-next"
+            href="https://github.com/Fredkiss3/gh-next/fork"
             variant="ghost"
             renderLeadingIcon={(cls) => (
               <RepoForkedIcon className={clsx(cls, "text-grey")} />
