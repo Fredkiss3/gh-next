b5993a184d1bb9ff92644647a215d004453adaea
Adrien KISSIE <fredkiss3@gmail.com>
2023-07-21 16:20:45 +0200

    💄 Show languages on homepage

--- src/app/(routes)/(app)/page.tsx
+++ src/app/(routes)/(app)/page.tsx
@@ -1,5 +1,7 @@
 // components
 import {
+  BookIcon,
+  DotFillIcon,
   EyeIcon,
   GlobeIcon,
   LinkIcon,
@@ -19,7 +21,11 @@ import { MarkdownContent } from "~/app/(components)/markdown-content";
 // utils
 import { getSession } from "~/app/(actions)/auth";
 import { getGithubRepoData } from "~/app/(actions)/github";
-import { AUTHOR_AVATAR_URL, GITHUB_AUTHOR_USERNAME } from "~/lib/constants";
+import {
+  AUTHOR_AVATAR_URL,
+  GITHUB_AUTHOR_USERNAME,
+  GITHUB_REPOSITORY_NAME,
+} from "~/lib/constants";
 import { clsx } from "~/lib/functions";
 
 export default async function Page() {
@@ -34,7 +40,8 @@ export default async function Page() {
         className={clsx(
           "pb-6 border-b border-neutral px-8",
           "hidden items-center justify-between flex-wrap",
-          "md:flex"
+          "md:flex",
+          "xl:px-0 xl:mx-8"
         )}
       >
         <h1 className="text-2xl font-semibold flex items-center gap-3">
@@ -151,26 +158,35 @@ export default async function Page() {
         </div>
 
         <ul className="flex items-center flex-wrap gap-4">
-          <li className="text-grey flex items-center gap-2">
-            <StarIcon className="h-4 w-4" />
-
-            <div>
-              <strong className="text-foreground">
-                {repositoryData.stargazerCount}
-              </strong>
-              &nbsp;
-              <span>stars</span>
-            </div>
+          <li>
+            <a
+              className="text-grey flex items-center gap-2 hover:text-accent group"
+              href={`https://github.com/${GITHUB_AUTHOR_USERNAME}/${GITHUB_REPOSITORY_NAME}/stargazers`}
+            >
+              <StarIcon className="h-4 w-4" />
+              <div>
+                <strong className="text-foreground group-hover:text-accent">
+                  {repositoryData.stargazerCount}
+                </strong>
+                &nbsp;
+                <span>stars</span>
+              </div>
+            </a>
           </li>
-          <li className="text-grey flex items-center gap-2">
-            <RepoForkedIcon className="h-4 w-4" />
-            <div>
-              <strong className="text-foreground">
-                {repositoryData.forkCount}
-              </strong>
-              &nbsp;
-              <span>fork</span>
-            </div>
+          <li>
+            <a
+              className="text-grey flex items-center gap-2 hover:text-accent group"
+              href={`https://github.com/${GITHUB_AUTHOR_USERNAME}/${GITHUB_REPOSITORY_NAME}/forks`}
+            >
+              <RepoForkedIcon className="h-4 w-4" />
+              <div>
+                <strong className="text-foreground group-hover:text-accent">
+                  {repositoryData.forkCount}
+                </strong>
+                &nbsp;
+                <span>fork</span>
+              </div>
+            </a>
           </li>
           <li className="text-grey flex items-center gap-2">
             <EyeIcon className="h-4 w-4" />
@@ -182,9 +198,14 @@ export default async function Page() {
               <span>watching</span>
             </div>
           </li>
-          <li className="text-grey flex items-center gap-2">
-            <PulseIcon className="h-4 w-4" />
-            <span>Activity</span>
+          <li>
+            <a
+              className="text-grey flex items-center gap-2 hover:text-accent group"
+              href={`https://github.com/${GITHUB_AUTHOR_USERNAME}/${GITHUB_REPOSITORY_NAME}/pulse`}
+            >
+              <PulseIcon className="h-4 w-4" />
+              <span>Activity</span>
+            </a>
           </li>
         </ul>
 
@@ -194,16 +215,141 @@ export default async function Page() {
         </div>
       </section>
 
-      <ReadmeContent />
+      <section
+        id="body"
+        className={clsx(
+          "grid items-start gap-4",
+          "sm:px-5",
+          "md:grid-cols-11 md:px-8",
+          "lg:grid-cols-[repeat(13,_minmax(0,_1fr))]",
+          "xl:grid-cols-[repeat(15,_minmax(0,_1fr))]"
+        )}
+      >
+        <ReadmeContent className="col-span-7 lg:col-span-9 xl:col-span-11" />
+
+        <aside className={clsx("hidden col-span-4 flex-col gap-6", "md:flex")}>
+          <div className="flex flex-col items-start gap-5 border-neutral pb-6 border-b">
+            <h2 className="text-lg font-semibold">About</h2>
+            <p className="text-lg">{repositoryData.description}</p>
+
+            <div className="flex gap-3 items-center">
+              <LinkIcon className="h-4 w-4" />
+              <a
+                href={repositoryData.url}
+                className="text-accent font-semibold"
+              >
+                {repositoryData.url}
+              </a>
+            </div>
+
+            <ul className="flex flex-col items-start gap-3">
+              <li>
+                <a
+                  href="#readme"
+                  className="text-grey flex items-center gap-3 hover:text-accent"
+                >
+                  <BookIcon className="h-4 w-4" />
+                  <span>Readme</span>
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href={`https://github.com/${GITHUB_AUTHOR_USERNAME}/${GITHUB_REPOSITORY_NAME}/pulse`}
+                  className="text-grey flex items-center gap-3 hover:text-accent"
+                >
+                  <PulseIcon className="h-4 w-4" />
+                  <span>Activity</span>
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href={`https://github.com/${GITHUB_AUTHOR_USERNAME}/${GITHUB_REPOSITORY_NAME}/stargazers`}
+                  className="text-grey flex items-center gap-3 hover:text-accent"
+                >
+                  <StarIcon className="h-4 w-4" />
+
+                  <div>
+                    <strong>{repositoryData.stargazerCount}</strong>
+                    &nbsp;
+                    <span>stars</span>
+                  </div>
+                </a>
+              </li>
+
+              <li className="text-grey flex items-center gap-3">
+                <EyeIcon className="h-4 w-4" />
+                <div>
+                  <strong>{repositoryData.watcherCount}</strong>
+                  &nbsp;
+                  <span>watching</span>
+                </div>
+              </li>
+
+              <li className="text-grey flex items-center gap-3">
+                <a
+                  href={`https://github.com/${GITHUB_AUTHOR_USERNAME}/${GITHUB_REPOSITORY_NAME}/forks`}
+                  className="text-grey flex items-center gap-3 hover:text-accent"
+                >
+                  <RepoForkedIcon className="h-4 w-4" />
+                  <div>
+                    <strong>{repositoryData.forkCount}</strong>
+                    &nbsp;
+                    <span>fork</span>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="flex flex-col items-start pb-6">
+            <h2 className="text-lg font-semibold mb-5">Languages</h2>
+            <div className="flex rounded-md h-2 w-full mb-3">
+              {repositoryData.languages.map((lang, index) => (
+                <div
+                  className={clsx("h-full", {
+                    "rounded-l-md": index === 0,
+                    "rounded-r-md":
+                      index === repositoryData.languages.length - 1,
+                    "border-l-2 border-neutral": index !== 0,
+                  })}
+                  style={{
+                    backgroundColor: lang.color,
+                    width: `${lang.percent}%`,
+                  }}
+                  key={lang.name}
+                />
+              ))}
+            </div>
+
+            <ul className="flex flex-wrap items-start gap-2">
+              {repositoryData.languages.map((lang) => (
+                <li key={lang.color} className="flex items-center gap-1">
+                  <div
+                    className="flex items-center justify-center"
+                    style={{
+                      color: lang.color,
+                    }}
+                  >
+                    <DotFillIcon className="h-4 w-4" />
+                  </div>
+                  <span className="font-semibold">{lang.name}</span>
+                  <span className="text-grey">{lang.percent}%</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </aside>
+      </section>
     </div>
   );
 }
 
-async function ReadmeContent() {
+async function ReadmeContent({ className }: { className?: string }) {
   const { readmeContent } = await getGithubRepoData();
 
   return (
-    <section className={clsx("sm:px-5", "md:px-8")}>
+    <div className={className} id="readme">
       <div
         className={clsx(
           "border border-neutral flex items-center gap-2 p-4",
@@ -229,6 +375,6 @@ async function ReadmeContent() {
           className="px-8 pb-8 pt-4"
         />
       </div>
-    </section>
+    </div>
   );
 }
