63e2e042f59adf5551562567d5f4f75a36effd95
Adrien KISSIE <fredkiss3@gmail.com>
2023-07-29 21:48:23 +0200

    💄 add issue-row skeleton component

--- www/src/app/(components)/issue-row-skeleton.tsx
+++ www/src/app/(components)/issue-row-skeleton.tsx
@@ -1,7 +1,23 @@
+import { IssueOpenedIcon } from "@primer/octicons-react";
 import * as React from "react";
+import { clsx } from "~/lib/functions";
 
 export type IssueRowSkeletonProps = {};
 
 export function IssueRowSkeleton({}: IssueRowSkeletonProps) {
-  return <></>;
+  return (
+    <div className="flex w-full gap-4 items-start p-5 border-b border-neutral/70">
+      <IssueOpenedIcon className="h-5 w-5 flex-shrink-0 text-success" />
+      <div className={clsx("flex flex-col items-start gap-2 w-full ")}>
+        <span className="sr-only">Loading issue...</span>
+        <div className="flex justify-between w-full gap-4">
+          <div className="bg-neutral h-4 rounded animate-pulse w-full sm:w-[70%]" />
+          <div className="hidden sm:block bg-neutral h-4 w-6 rounded animate-pulse" />
+        </div>
+
+        <div className="bg-neutral h-4 rounded animate-pulse w-[60%] sm:w-1/5" />
+        <div className="bg-neutral h-2 rounded animate-pulse w-[55%] sm:w-1/2" />
+      </div>
+    </div>
+  );
 }
--- www/src/app/(routes)/(app)/issues/page.tsx
+++ www/src/app/(routes)/(app)/issues/page.tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 // components
 import {
   CheckIcon,
@@ -13,17 +14,18 @@ import { Button } from "~/app/(components)/button";
 import { CounterBadge } from "~/app/(components)/counter-badge";
 import { IssuesListHeaderForm } from "~/app/(components)/issues-list-header-form";
 import { SegmentedLayout } from "~/app/(components)/segmented-layout";
-
-// utils
-import { clsx } from "~/lib/functions";
-
-// types
-import type { Metadata } from "next";
-import type { PageProps } from "~/lib/types";
 import { IssueAuthorFilterActionList } from "~/app/(components)/issue-author-filter-action-list";
 import { IssueLabelFilterActionList } from "~/app/(components)/issue-label-filter-action-list";
 import { IssueAssigneeFilterActionList } from "~/app/(components)/issue-assignee-filter-action-list";
 import { IssueSortActionList } from "~/app/(components)/issue-sort-action-list";
+import { IssueRowSkeleton } from "~/app/(components)/issue-row-skeleton";
+
+// utils
+import { clsx, wait } from "~/lib/functions";
+
+// types
+import type { Metadata } from "next";
+import type { PageProps } from "~/lib/types";
 
 export const metadata: Metadata = {
   title: "Issues",
@@ -81,6 +83,167 @@ function IssuesListHeader() {
 async function IssuesListBody(props: { params: PageProps["searchParams"] }) {
   return (
     <section className="flex flex-col gap-4" id="issue-list">
+      <React.Suspense fallback={<IssueContentTableSkeleton />}>
+        <IssueContentTable />
+      </React.Suspense>
+
+      <div className="px-5 md:px-0 justify-center text-grey flex items-start gap-2 py-12">
+        <LightBulbIcon className="h-5 w-5" />
+        <p>
+          <strong className="text-foreground">ProTip!</strong> Ears burning? Get
+          mentions with&nbsp;
+          <Link href="/issues?q=is:open+mention:@me" className="text-accent">
+            mentions:@me.
+          </Link>
+        </p>
+      </div>
+    </section>
+  );
+}
+
+function IssueContentTableSkeleton() {
+  return (
+    <>
+      {/* Header */}
+      <div className="flex flex-col gap-4 px-5 md:px-0">
+        <div className="flex items-center gap-4 md:hidden">
+          <Link
+            href="/issues?q=is:open"
+            className={clsx(
+              "font-semibold text-foreground flex items-center gap-2"
+            )}
+          >
+            <IssueOpenedIcon className="h-5 w-5" />
+            <p>
+              - <span className="sr-only">issues</span>&nbsp;Open
+            </p>
+          </Link>
+          <Link
+            href="/issues?q=is:closed"
+            className={clsx("text-grey flex items-center gap-2")}
+          >
+            <CheckIcon className="h-5 w-5" />
+            <span>
+              - <span className="sr-only">issues</span>&nbsp;Closed
+            </span>
+          </Link>
+        </div>
+      </div>
+
+      <div className={clsx("border border-neutral", "sm:rounded-md")}>
+        {/* Issue content table - header */}
+        <div
+          className={clsx(
+            "flex items-center justify-between gap-8",
+            "p-5 border-b border-neutral bg-subtle text-grey"
+          )}
+        >
+          <ul className="hidden md:flex items-center gap-4">
+            <li>
+              <Link
+                href="/issues?q=is:open"
+                className={clsx(
+                  "font-semibold text-foreground flex items-center gap-2"
+                )}
+              >
+                <IssueOpenedIcon className="h-5 w-5" />
+                <p>
+                  - <span className="sr-only">issues</span>&nbsp;Open
+                </p>
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/issues?q=is:closed"
+                className={clsx("text-grey flex items-center gap-2")}
+              >
+                <CheckIcon className="h-5 w-5" />
+                <span>
+                  - <span className="sr-only">issues</span>&nbsp;Closed
+                </span>
+              </Link>
+            </li>
+          </ul>
+
+          <ul
+            className={clsx(
+              "flex items-center gap-6 justify-evenly flex-grow",
+              "sm:justify-start",
+              "md:flex-grow-0"
+            )}
+          >
+            <li>
+              <IssueAuthorFilterActionList>
+                <button className="flex items-center gap-2">
+                  <span>Author</span> <TriangleDownIcon className="h-5 w-5" />
+                </button>
+              </IssueAuthorFilterActionList>
+            </li>
+            <li>
+              <IssueLabelFilterActionList>
+                <button className="flex items-center gap-2">
+                  <span>Label</span> <TriangleDownIcon className="h-5 w-5" />
+                </button>
+              </IssueLabelFilterActionList>
+            </li>
+            <li>
+              <IssueAssigneeFilterActionList>
+                <button className="flex items-center gap-2">
+                  <span>Assignee</span> <TriangleDownIcon className="h-5 w-5" />
+                </button>
+              </IssueAssigneeFilterActionList>
+            </li>
+            <li>
+              <IssueSortActionList>
+                <button className="flex items-center gap-2">
+                  <span>Sort</span> <TriangleDownIcon className="h-5 w-5" />
+                </button>
+              </IssueSortActionList>
+            </li>
+          </ul>
+        </div>
+        {/* END Issue content table - header */}
+
+        {/* Issue content table - list */}
+        <ul>
+          <li>
+            <IssueRowSkeleton />
+          </li>
+          <li>
+            <IssueRowSkeleton />
+          </li>
+          <li>
+            <IssueRowSkeleton />
+          </li>
+          <li>
+            <IssueRowSkeleton />
+          </li>
+          <li>
+            <IssueRowSkeleton />
+          </li>
+          <li>
+            <IssueRowSkeleton />
+          </li>
+          <li>
+            <IssueRowSkeleton />
+          </li>
+          <li>
+            <IssueRowSkeleton />
+          </li>
+          <li>
+            <IssueRowSkeleton />
+          </li>
+        </ul>
+        {/* END Issue content table - list */}
+      </div>
+    </>
+  );
+}
+async function IssueContentTable() {
+  await wait(2000);
+  return (
+    <>
+      {/* Header */}
       <div className="flex flex-col gap-4 px-5 md:px-0">
         <Link
           href="/issues"
@@ -116,8 +279,7 @@ async function IssuesListBody(props: { params: PageProps["searchParams"] }) {
         </div>
       </div>
 
-      {/* Issue content table */}
-      <div className={clsx("border border-neutral", "md:rounded-md")}>
+      <div className={clsx("border border-neutral", "sm:rounded-md")}>
         {/* Issue content table - header */}
         <div
           className={clsx(
@@ -193,20 +355,10 @@ async function IssuesListBody(props: { params: PageProps["searchParams"] }) {
 
         {/* Issue content table - list */}
         <EmptyState />
+
         {/* END Issue content table - list */}
       </div>
-
-      <div className="px-5 md:px-0 justify-center text-grey flex items-start gap-2 py-12">
-        <LightBulbIcon className="h-5 w-5" />
-        <p>
-          <strong className="text-foreground">ProTip!</strong> Ears burning? Get
-          mentions with&nbsp;
-          <Link href="/issues?q=is:open+mention:@me" className="text-accent">
-            mentions:@me.
-          </Link>
-        </p>
-      </div>
-    </section>
+    </>
   );
 }
 
