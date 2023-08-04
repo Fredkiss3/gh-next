2ff4aa21c2e2b12b848f36fea64e1bd3bd1dc459
Adrien KISSIE <fredkiss3@gmail.com>
2023-08-04 11:31:07 +0200

    🔧 enable react strict mode

--- www/next.config.mjs
+++ www/next.config.mjs
@@ -3,6 +3,7 @@ import "./src/env.mjs";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   experimental: {
     serverActions: true,
     logging: "verbose",
