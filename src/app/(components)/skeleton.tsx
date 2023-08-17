95f9eac1d13fb37b6a86115756d603df6ccf86af
Adrien KISSIE <fredkiss3@gmail.com>
2023-08-17 14:19:39 +0200

    🚧 WIP : add loading.tsx to homepage to unblock the rendering

---
+++ src/app/(components)/skeleton.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { clsx } from "~/lib/shared-utils";
+
+export type SkeletonProps = {
+  className?: string;
+  shape?: "rectangle" | "circle";
+};
+
+export function Skeleton({ className, shape = "rectangle" }: SkeletonProps) {
+  return (
+    <div
+      className={clsx(className, "animate-pulse bg-grey/60", {
+        "rounded-md": shape === "rectangle",
+        "rounded-full": shape === "circle",
+      })}
+    />
+  );
+}
