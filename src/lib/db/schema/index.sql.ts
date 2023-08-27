835dffbaae08dfb5c8203ea180f9b303a4df611c
Adrien KISSIE <fredkiss3@gmail.com>
2023-08-27 06:34:58 +0200

    ♻️ some changes

--- src/lib/db/schema/index.sql.ts
+++ src/lib/db/schema/index.sql.ts
@@ -1,3 +1,3 @@
-import { serial, text, pgTableCreator } from "drizzle-orm/pg-core";
+import { pgTableCreator } from "drizzle-orm/pg-core";
 
 export const pgTable = pgTableCreator((name) => `gh_next_${name}`);
