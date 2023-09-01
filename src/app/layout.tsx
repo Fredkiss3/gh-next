d4395d6f74a3dde92ed39e571ce94f8bd7773d29
Adrien KISSIE <fredkiss3@gmail.com>
2023-09-01 12:14:41 +0200

    Feat/dark light favicon (#49)
    * ✨ added dark & light favicon
    
    * 🔥 remove unused assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import "./globals.css";
+import * as React from "react";
 
 // components
 import { TailwindIndicator } from "~/app/(components)/tailwind-indicator";
 import { Toaster } from "~/app/(components)/toast/toaster.server";
 import NextTopLoader from "nextjs-toploader";
+import { IconSwitcher } from "~/app/(components)/icon-switcher";
 
 // utils
 import { Inter } from "next/font/google";
@@ -32,12 +34,29 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const theme = await getTheme();
   return (
-    <html lang="en" suppressHydrationWarning data-theme={await getTheme()}>
+    <html lang="en" suppressHydrationWarning data-theme={theme}>
+      <head>
+        <link
+          rel="alternate icon"
+          type="image/png"
+          href={theme === "dark" ? `/favicon-dark.png` : `/favicon.png`}
+          data-favicon
+        />
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href={theme === "dark" ? `/favicon-dark.svg` : `/favicon.svg`}
+          data-favicon
+        />
+      </head>
       <body
         className={clsx(inter.className, "bg-backdrop")}
         suppressHydrationWarning
       >
+        <IconSwitcher />
+
         <NextTopLoader showSpinner={false} />
         {children}
         {process.env.NODE_ENV !== "production" && <TailwindIndicator />}
