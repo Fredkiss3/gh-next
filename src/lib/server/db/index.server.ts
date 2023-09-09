b571bde6effede46f681eddb58a9296f846fea05
Adrien KISSIE <fredkiss3@gmail.com>
2023-09-09 17:42:45 +0200

    🐛  fix max connection errors with postgres

--- src/lib/server/db/index.server.ts
+++ src/lib/server/db/index.server.ts
@@ -35,10 +35,16 @@ import {
 } from "./schema/activity.sql";
 import { reactions, reactionsRelations } from "./schema/reaction.sql";
 import { env } from "~/env.mjs";
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
-export const db = drizzle(postgres(env.DATABASE_URL), {
+const pool = new Pool({
+  connectionString: env.DATABASE_URL,
+  min: 1,
+  max: 5
+});
+
+export const db = drizzle(pool, {
   logger: true,
   schema: {
     users,
