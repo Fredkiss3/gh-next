c768c6fb4d32b2fbabb6ac8134379212add5f0e4
Adrien KISSIE <fredkiss3@gmail.com>
2023-09-11 05:10:48 +0200

    💄 update account settings loading UI to reflect the new changes

--- src/app/(routes)/(app)/(settings)/(pages)/settings/account/loading.tsx
+++ src/app/(routes)/(app)/(settings)/(pages)/settings/account/loading.tsx
@@ -2,11 +2,17 @@ import { Skeleton } from "~/app/(components)/skeleton";
 
 export default function Loading() {
   return (
-    <div className="flex flex-col gap-4">
-      <section className="flex flex-col gap-3 md:gap-5">
+    <div className="flex flex-col gap-24">
+      <section className="flex flex-col gap-4 md:gap-8">
         <Skeleton className="h-14 border-b border-neutral" />
 
-        <Skeleton className="h-12 border-b border-neutral" />
+        <Skeleton className="h-24 border-b border-neutral" />
+
+        <Skeleton className="h-[300px] border-b border-neutral" />
+      </section>
+
+      <section className="flex flex-col gap-4 md:gap-8">
+        <Skeleton className="h-14 border-b border-neutral" />
 
         <Skeleton className="h-32 border-b border-neutral" />
       </section>
