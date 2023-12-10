473a2bfe2b2f43bdbd08edb7d6d94c5390a259aa
Adrien KISSIE <fredkiss3@gmail.com>
2023-12-10 08:41:26 +0100

    🚧 text resizing & dropdown toolbar items done

--- src/app/(components)/issues/markdown-text-area.tsx
+++ src/app/(components)/issues/markdown-text-area.tsx
@@ -10,6 +10,7 @@ import {
   DiffIgnoredIcon,
   HeadingIcon,
   ItalicIcon,
+  KebabHorizontalIcon,
   LinkIcon,
   ListOrderedIcon,
   ListUnorderedIcon,
@@ -27,6 +28,13 @@ import { clsx } from "~/lib/shared/utils.shared";
 import type { TextareaProps } from "~/app/(components)/textarea";
 import { Button } from "~/app/(components)/button";
 import { Tooltip } from "~/app/(components)/tooltip";
+import {
+  DropdownContent,
+  DropdownItem,
+  DropdownRoot,
+  DropdownSeparator,
+  DropdownTrigger
+} from "~/app/(components)/dropdown";
 
 export type MarkdownTextAreaProps = Omit<TextareaProps, "value">;
 
@@ -43,6 +51,27 @@ export function MarkdownTextArea({
 }: MarkdownTextAreaProps) {
   const [textContent, setTextContent] = React.useState(defaultValue);
   const [selectedTab, setSelectedTab] = React.useState<TabValue>(TABS.EDITOR);
+  const [textAreaHeight, setTextAreaHeight] = React.useState(0);
+  const textAreaRef = React.useRef<React.ElementRef<"textarea">>(null);
+
+  React.useEffect(() => {
+    const observer = new ResizeObserver(([entry]) => {
+      const height = (entry.target as HTMLTextAreaElement).offsetHeight;
+      // height becomes 0 when switching tabs
+      if (height > 0) {
+        setTextAreaHeight(height);
+      }
+    });
+
+    const textarea = textAreaRef.current;
+    if (textarea) {
+      setTextAreaHeight(textarea.offsetHeight);
+      observer.observe(textarea);
+      return () => {
+        observer.unobserve(textarea);
+      };
+    }
+  }, [selectedTab]);
 
   return (
     <div className="flex flex-col gap-1">
@@ -59,7 +88,7 @@ export function MarkdownTextArea({
             <Tabs.Trigger
               value={TABS.EDITOR}
               className={clsx(
-                "px-4 py-2 border-b border-neutral",
+                "px-3 py-2 border-b border-neutral",
                 "aria-[selected=true]:rounded-t-md aria-[selected=true]:border",
                 "aria-[selected=true]:border-b-0 aria-[selected=true]:border-l-0",
                 "aria-[selected=true]:bg-background"
@@ -70,7 +99,7 @@ export function MarkdownTextArea({
             <Tabs.Trigger
               value={TABS.PREVIEW}
               className={clsx(
-                "px-4 py-2 border-b border-neutral",
+                "px-3 py-2 border-b border-neutral",
                 "aria-[selected=true]:rounded-t-md aria-[selected=true]:border",
                 "aria-[selected=true]:border-b-0",
                 "aria-[selected=true]:bg-background"
@@ -101,7 +130,7 @@ export function MarkdownTextArea({
                 label="Link"
                 onClick={() => {}}
                 icon={LinkIcon}
-                className="hidden sm:inline-flex"
+                className="hidden lg:inline-flex"
               />
 
               <Toolbar.Separator className="h-4 self-center bg-neutral/40 w-[1px] mx-2 hidden lg:block" />
@@ -145,6 +174,57 @@ export function MarkdownTextArea({
                 icon={DiffIgnoredIcon}
                 className="hidden lg:inline-flex"
               />
+
+              <DropdownRoot>
+                <Toolbar.Button asChild>
+                  <DropdownTrigger>
+                    <Button isSquared variant="neutral" className="lg:hidden">
+                      <span className="sr-only">More actions</span>
+                      <KebabHorizontalIcon className="h-4 w-4 text-grey" />
+                    </Button>
+                  </DropdownTrigger>
+                </Toolbar.Button>
+
+                <DropdownContent align="end">
+                  <DropdownItem
+                    icon={LinkIcon}
+                    text="Link"
+                    onClick={() => {}}
+                  />
+                  <DropdownSeparator />
+                  <DropdownItem
+                    text="Numbered list"
+                    onClick={() => {}}
+                    icon={ListOrderedIcon}
+                  />
+                  <DropdownItem
+                    text="Unordered list"
+                    onClick={(e) => {}}
+                    icon={ListUnorderedIcon}
+                  />
+                  <DropdownItem
+                    text="Task list"
+                    onClick={() => {}}
+                    icon={TasklistIcon}
+                  />
+                  <DropdownSeparator />
+                  <DropdownItem
+                    text="Mentions"
+                    onClick={() => {}}
+                    icon={MentionIcon}
+                  />
+                  <DropdownItem
+                    text="References"
+                    onClick={() => {}}
+                    icon={CrossReferenceIcon}
+                  />
+                  <DropdownItem
+                    text="Slash commands"
+                    onClick={() => {}}
+                    icon={DiffIgnoredIcon}
+                  />
+                </DropdownContent>
+              </DropdownRoot>
             </Toolbar.Root>
           </Tabs.List>
 
@@ -172,9 +252,20 @@ export function MarkdownTextArea({
                 className="text-sm"
                 name={selectedTab === "EDITOR" ? props.name : ""}
                 hideLabel
+                ref={textAreaRef}
+                style={{
+                  height: textAreaHeight
+                }}
               />
             </Tabs.Content>
-            <Tabs.Content value={TABS.PREVIEW}>Preview</Tabs.Content>
+            <Tabs.Content
+              value={TABS.PREVIEW}
+              style={{
+                height: textAreaHeight
+              }}
+            >
+              Preview
+            </Tabs.Content>
           </div>
         </Tabs.Root>
 
@@ -217,17 +308,17 @@ function ToolbarButton({
 }: ToolbarButtonProps) {
   return (
     <Toolbar.Button {...props} asChild>
-      <Tooltip content={label} placement="bottom" className="text-xs">
-        <Button
-          onClick={onClick}
-          isSquared
-          variant="neutral"
-          className={clsx(className)}
-        >
-          <span className="sr-only">{label}</span>
-          <Icon className="h-4 w-4 text-grey" />
-        </Button>
-      </Tooltip>
+      {/* <Tooltip content={label} placement="bottom" className="text-xs"> */}
+      <Button
+        onClick={onClick}
+        isSquared
+        variant="neutral"
+        className={clsx(className)}
+      >
+        <span className="sr-only">{label}</span>
+        <Icon className="h-4 w-4 text-grey" />
+      </Button>
+      {/* </Tooltip> */}
     </Toolbar.Button>
   );
 }
