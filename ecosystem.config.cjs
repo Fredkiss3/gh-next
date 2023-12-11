bb4d6846fb1780fa91f8380dc572c71490705612
Adrien KISSIE <fredkiss3@gmail.com>
2023-12-11 15:54:49 +0100

    🔧  some PM2 configs improvements

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,11 +6,13 @@ module.exports = {
       time: true,
       instances: 2,
       autorestart: true,
-      max_restarts: 10,
+      max_restarts: 5,
       exec_mode: "cluster_mode",
       watch: false,
-      instance_var: "INSTANCE_ID",
       max_memory_restart: "1G",
+      wait_ready: true,
+      listen_timeout: 10_000,
+      increment_var: "PORT",
       env: {
         PORT: 8892,
         HOSTNAME: "0.0.0.0"
