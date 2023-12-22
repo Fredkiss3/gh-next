c5fcc0c6cc5576eb62c19fa556f8cab64a49cfb0
Adrien KISSIE <fredkiss3@gmail.com>
2023-12-22 02:42:19 +0100

    ♻️ refactor

--- dc-build-local.sh
+++ dc-build-local.sh
@@ -1,4 +1,4 @@
 while read -r line; do
     build_args="$build_args --build-arg $line"
 done < .env.docker.local
-docker build $build_args -t gh-next:latest -f docker/Dockerfile.dev . 
+docker build $build_args -t dcr.fredkiss.dev/gh-next:latest -f docker/Dockerfile.dev . 
