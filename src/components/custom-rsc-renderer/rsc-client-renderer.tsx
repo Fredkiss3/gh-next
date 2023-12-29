f1ca2c00fee995c7325b0437145e9d914431db46
Adrien KISSIE <fredkiss3@gmail.com>
2023-12-29 22:37:47 +0100

    ♻️ make rsc-client-renderer more robust

--- src/components/custom-rsc-renderer/rsc-client-renderer.tsx
+++ src/components/custom-rsc-renderer/rsc-client-renderer.tsx
@@ -4,7 +4,6 @@ import * as RSDWSSr from "react-server-dom-webpack/client.edge";
 import * as RSDW from "react-server-dom-webpack/client";
 
 import { getSSRManifest } from "./rsc-manifest";
-import { fnCache } from "~/lib/shared/fn-cache";
 
 export type RscClientRendererProps = {
   payloadOrPromise: string | Promise<string>;
@@ -15,40 +14,75 @@ export function RscClientRenderer({
   payloadOrPromise,
   withSSR = false
 }: RscClientRendererProps) {
-  return React.use(renderPayloadOrPromiseToJSX(payloadOrPromise, withSSR));
+  const renderPromise = React.useMemo(() => {
+    /**
+     * This is to fix a bug that happens sometimes in the SSR phase,
+     * calling `use` seems to suspend indefinitely resulting in
+     * your app not responding.
+     *
+     * We override the promise result and add `status` & `value`,
+     * these fields are used internally by `use` and are what's
+     * prevent `use` from suspending indefinitely.
+     */
+    const pendingPromise = resolveElement(payloadOrPromise, withSSR)
+      .then((value) => {
+        // @ts-expect-error
+        if (pendingPromise.status === "pending") {
+          const fulfilledThenable = pendingPromise as any;
+          fulfilledThenable.status = "fulfilled";
+          fulfilledThenable.value = value;
+        }
+        return value;
+      })
+      .catch((error) => {
+        // @ts-expect-error
+        if (pendingPromise.status === "pending") {
+          const rejectedThenable = pendingPromise as any;
+          rejectedThenable.status = "rejected";
+          rejectedThenable.reason = error;
+        }
+        throw error;
+      });
+    // @ts-expect-error
+    pendingPromise.status = "pending";
+    return pendingPromise;
+  }, [payloadOrPromise, withSSR]);
+
+  return <RscClientRendererUse promise={renderPromise} />;
 }
 
-export const renderPayloadOrPromiseToJSX = fnCache(
-  async function resolveElement(
-    payloadOrPromise: string | Promise<string>,
-    ssr = false
-  ) {
-    console.log("Render payload to JSX");
-    const payload =
-      typeof payloadOrPromise === "string"
-        ? payloadOrPromise
-        : await payloadOrPromise;
-    const rscStream = transformStringToReadableStream(payload);
-    let rscPromise: Promise<React.JSX.Element> | null = null;
+function RscClientRendererUse(props: {
+  promise: Promise<React.JSX.Element>;
+}) {
+  return React.use(props.promise);
+}
 
-    // Render to HTML
-    if (ssr && typeof window === "undefined") {
-      // the SSR manifest contains all the client components that will be SSR'ed
-      // And also how to import them
-      rscPromise = RSDWSSr.createFromReadableStream(
-        rscStream,
-        getSSRManifest()
-      );
-    }
+async function resolveElement(
+  payloadOrPromise: string | Promise<string>,
+  ssr = false
+) {
+  console.log("Render payload to JSX");
+  const payload =
+    typeof payloadOrPromise === "string"
+      ? payloadOrPromise
+      : await payloadOrPromise;
+  const rscStream = transformStringToReadableStream(payload);
+  let rscPromise: Promise<React.JSX.Element> | null = null;
 
-    // Hydrate or CSR
-    if (rscPromise === null) {
-      rscPromise = RSDW.createFromReadableStream(rscStream, {});
-    }
-
-    return await rscPromise;
+  // Render to HTML
+  if (ssr && typeof window === "undefined") {
+    // the SSR manifest contains all the client components that will be SSR'ed
+    // And also how to import them
+    rscPromise = RSDWSSr.createFromReadableStream(rscStream, getSSRManifest());
   }
-);
+
+  // Hydrate or CSR
+  if (rscPromise === null) {
+    rscPromise = RSDW.createFromReadableStream(rscStream, {});
+  }
+
+  return await rscPromise;
+}
 
 export function transformStringToReadableStream(input: string) {
   // Using Flight to deserialize the args from the string.
