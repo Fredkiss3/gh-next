88834a14c39d04446f0d7142c56c8acbd38f6bc7
Adrien KISSIE <fredkiss3@gmail.com>
2024-01-02 04:21:52 +0100

    ⚗️ another try

--- dc-build-local.sh
+++ dc-build-local.sh
@@ -1,4 +1,4 @@
 while read -r line; do
     build_args="$build_args --build-arg $line"
 done < .env.docker.local
-docker build $build_args -t dcr.fredkiss.dev/gh-next:latest -f docker/Dockerfile.dev . 
+docker buildx build --push -t dcr.fredkiss.dev/gh-next:latest -f docker/Dockerfile.dev $build_args --cache-from type=registry,ref=dcr.fredkiss.dev/gh-next:prod-buildcache,mode=max --cache-to type=registry,ref=dcr.fredkiss.dev/gh-next:prod-buildcache,mode=max .
