6d45a438593a12a9f6773cd0383fa6c068fe1353
Adrien KISSIE <fredkiss3@gmail.com>
2024-01-20 10:35:15 +0100

    👷 dev DB & PROD DB

--- migrate.ts
+++ migrate.ts
@@ -2,7 +2,9 @@ import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 
-const db = drizzle(postgres(process.env.DATABASE_URL!));
+const db = drizzle(
+  postgres(process.env.REMOTE_DATABASE_URL ?? process.env.DATABASE_URL!)
+);
 
 async function main() {
   await migrate(db, { migrationsFolder: "drizzle" });
