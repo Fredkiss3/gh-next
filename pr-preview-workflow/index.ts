16f30f9fb7a315dd6b87bcd9d3d926a52749dbdd
Adrien KISSIE <fredkiss3@gmail.com>
2024-01-28 01:14:24 +0100

    🔨 added new project for preview environment workflow setup

--- pr-preview-workflow/index.ts
+++ pr-preview-workflow/index.ts
@@ -1,38 +1,64 @@
-import { $, file } from "bun";
+import { parseArgs } from "node:util";
+import { z } from "zod";
+import { addDockerApp } from "./add-docker-app";
+import { addCaddyfile } from "./add-caddyfile";
 
-let input = "";
-// read stdin
-for await (const chunk of Bun.stdin.stream()) {
-  // chunk is Uint8Array
-  // this converts it to text (assumes ASCII encoding)
-  input += Buffer.from(chunk).toString();
-}
-console.log(`Chunk: ${input}`);
+const argSchema = z.object({
+  "pr-id": z.coerce.number(),
+  "pr-branch": z.string(),
+  "caddy-config-path": z.string(),
+  "reload-caddy": z.boolean(),
+  "reload-docker": z.boolean()
+});
 
-// TODO
-/**
- * process validate input for
- * - PR-ID, PR-BRANCH
- * - Read list of files inside caddy folder
- * - check if...
- */
+const { values, positionals } = parseArgs({
+  args: Bun.argv,
+  options: {
+    "pr-id": {
+      type: "string"
+    },
+    "pr-branch": {
+      type: "string"
+    },
+    "caddy-config-path": {
+      type: "string",
+      default: "./caddy-test" // for testing locally
+    },
+    "reload-caddy": {
+      type: "boolean",
+      default: false
+    },
+    "reload-docker": {
+      type: "boolean",
+      default: false
+    }
+  },
+  strict: true,
+  allowPositionals: true
+});
 
-// const CADDY_TEMPLATE_CONTENT = `gh-${PR_ID}.gh.fredkiss.dev, gh-${PR_BRANCH}.gh.fredkiss.dev {
-//     route {
-//        sablier localhost:10000 {
-//          group gh-next-${PR_ID}
-//          session_duration 30m
-//          dynamic {
-//             theme ghost
-//             display_name gh-next-${PR_ID}
-//             refresh_frequency 5s
-//          }
-//        }
+const {
+  "pr-id": PR_ID,
+  "pr-branch": PR_BRANCH,
+  "caddy-config-path": CADDY_CONFIG_FOLDER_PATH,
+  "reload-caddy": shouldReloadCaddy,
+  "reload-docker": shouldReloadDockerStack
+} = argSchema.parse(values);
 
-//       reverse_proxy gh-next-${PR_ID}:3000
-//     }
-//     log
-// }`;
+const MAX_OPEN_PORTS = 100,
+  STARTING_PORT_RANGE = 9100;
 
-// reload caddy service in docker
-// await $`docker exec $(docker ps -q -f name=caddy-stack_proxy) ls /etc/caddy`;
+await addDockerApp(
+  PR_ID,
+  shouldReloadDockerStack,
+  MAX_OPEN_PORTS,
+  STARTING_PORT_RANGE
+);
+await addCaddyfile(
+  PR_ID,
+  PR_BRANCH,
+  CADDY_CONFIG_FOLDER_PATH,
+  shouldReloadCaddy,
+  MAX_OPEN_PORTS,
+  STARTING_PORT_RANGE
+);
